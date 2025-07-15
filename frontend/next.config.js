145ac69f8148ce9e8a7f7d9ad17a76f3b57201d1
Shido-Droid <kcso20220270@gmail.com>
2025-07-15 10:53:20 +0900

    Add webpack alias config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,10 @@ const path = require('path');
 
 module.exports = {
   webpack(config) {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '@': path.resolve(__dirname, 'src'),
+    };
     return config;
   },
 };
